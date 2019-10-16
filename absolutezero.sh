1f41208932287f607de5bccb99a2737f968d95f5
John Meyerhoff <John@r73.de>
2019-10-16 15:42:20 +0200

    Fixing with cp :)

---
+++ absolutezero.sh
@@ -0,0 +1,11 @@
+php --version
+php --ini
+composer --version
+composer check-platform-reqs
+cp ./.env.example ./.env
+php artisan key:generate
+composer update
+./overviewlaravel.sh
+./rebuildDB.sh
+#open ./redirectafter3seconds.html
+php artisan serve
